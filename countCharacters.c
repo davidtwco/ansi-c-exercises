d6edb938e44070d38c70e83a1783f51a1d49d983
David Wood <david@davidtw.co>
2017-08-06 15:36:33 +0100

    Added second version of character counting.

--- countCharacters.c
+++ countCharacters.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-/* count characters in input; 1st version */
+/* count characters in input; 2nd version */
 main() {
-    long nc;
+    double nc;
 
-    nc = 0;
-    while (getchar() != EOF)
-        ++nc;
-    printf("%ld\n", nc);
+    for (nc = 0; getchar() != EOF; ++nc)
+        ;
+    printf("%.0f\n", nc);
 }
