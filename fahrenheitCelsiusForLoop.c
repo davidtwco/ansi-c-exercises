46a975120e7b5c93d0a10ce9472642f5b508aa5a
David Wood <david@davidtw.co>
2017-08-06 15:15:19 +0100

    [Ex1-5] Modify the temperature conversion program to print the table in reverse order, that is, from 300 degrees to 0.

--- fahrenheitCelsiusForLoop.c
+++ fahrenheitCelsiusForLoop.c
@@ -4,6 +4,6 @@
 main() {
     int fahr;
 
-    for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+    for (fahr = 300; fahr >= 0; fahr = fahr - 20)
         printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }
