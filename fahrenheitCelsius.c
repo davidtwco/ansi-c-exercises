680aee911b8313a581acadc91f5a0b4bad529c73
David Wood <david@davidtw.co>
2017-08-06 14:42:32 +0100

    [Ex1-3] Modify the temperature conversion program to print a heading above a table.

--- fahrenheitCelsius.c
+++ fahrenheitCelsius.c
@@ -10,10 +10,12 @@ main() {
     upper = 300; /* upper limit */
     step = 20; /* step size */
 
+    printf("Fahrenheit\tCelsius\n");
+
     fahr = lower;
     while (fahr <= upper) {
         celsius = (5.0 / 9.0) * (fahr-32.0);
-        printf("%3.0f\t%6.1f\n", fahr, celsius);
+        printf("%10.0f\t%7.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
