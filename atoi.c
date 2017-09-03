a0e2f5965734533f936715a4a8a1c832d19c29fd
David Wood <david@davidtw.co>
2017-09-03 21:55:51 +0100

    Updated atoi example.

--- atoi.c
+++ atoi.c
@@ -1,10 +1,16 @@
-/* example from Ch 2, Sec 2.7, Pg 43 */
-/* atoi: convert s to integer */
-int atoi(char s[]) {
-    int i, n;
+#include <ctype.h>
 
-    n = 0;
-    for (i = 0; s[i] >= '0' && s[i] <= '9'; ++i)
+/* example from Ch 3, Sec 3.5, Pg 61 */
+/* atoi: convert s to integer; version 2 */
+int atoi(char s[]) {
+    int i, n, sign;
+
+    for (i = 0; isspace(s[i]); i++) /* skip white space */
+        ;
+    sign = (s[i] == '-') ? -1 : 1;
+    if (s[i] == '+' || s[i] == '-') /* skip sign */
+        i++;
+    for (n = 0; isdigit(s[i]); i++)
         n = 10 * n + (s[i] - '0');
-    return n;
+    return sign * n;
 }
