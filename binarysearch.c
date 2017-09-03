4650c689f2f5de2d71598c2720bb539c14235749
David Wood <david@davidtw.co>
2017-09-03 20:14:40 +0100

    Added binary search example.

---
+++ binarysearch.c
@@ -0,0 +1,18 @@
+/* example from Ch 3, Sec 3.3, Pg 58 */
+/* binsearch: find x in v[0] <= v[1] <= ... <= v[n-1] */
+int binsearch(int x, int v[], int n) {
+    int low, high, mid;
+
+    low = 0;
+    high = n - 1;
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (x < v[mid])
+            high = mid - 1;
+        else if (x > v[mid])
+            low = mid + 1;
+        else /* found match */
+            return mid;
+    }
+    return -1; /* no match */
+}
