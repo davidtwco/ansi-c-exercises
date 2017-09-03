a70fca9ca3a00014b8ede3e4321e15dd5db981e3
David Wood <david@davidtw.co>
2017-09-03 18:05:21 +0100

    Added squeeze example.

---
+++ squeeze.c
@@ -0,0 +1,10 @@
+/* example from Ch 2, Sec 2.8, Pg 47 */
+/* squeeze: delete all c from s */
+void squeeze(char s[], int c) {
+    int i, j;
+
+    for (i = j = 0; s[i] != '\0'; i++)
+        if (s[i] != c)
+            s[j++] = s[i];
+    s[j] = '\0';
+}
