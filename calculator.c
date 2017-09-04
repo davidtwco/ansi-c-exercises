7145d82f6d929e9dabe09b7a6b1a8fee8c5ac716
David Wood <david@davidtw.co>
2017-09-04 21:27:09 +0100

    Added improved calculator example.

--- calculator.c
+++ calculator.c
@@ -1,56 +1,117 @@
-#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h> /* for atof() */
 
-#define MAXLINE 100
+#define MAXOP 100 /* max size of operand or operator */
+#define NUMBER '0' /* signal that a number was found */
 
-/* example from Ch 4, Sec 4.2, Pg 72 */
-/* a rudimentary calculator */
+int getop(char[]);
+void push(double);
+double pop(void);
+
+/* example from Ch 4, Sec 4.3, Pg 76, 77, 78, 79 */
+/* reverse Polish calculator */
 int main() {
-    double sum, atof(char[]);
-    char line[MAXLINE];
-    int _getline(char line[], int max);
+    int type;
+    double op2;
+    char s[MAXOP];
 
-    sum = 0;
-    while (_getline(line, MAXLINE) > 0)
-        printf("\t%g\n", sum += atof(line));
+    while ((type = getop(s)) != EOF) {
+        switch (type) {
+            case NUMBER:
+                push(atof(s));
+                break;
+            case '+':
+                push(pop() + pop());
+                break;
+            case '*':
+                push(pop() * pop());
+                break;
+            case '-':
+                op2 = pop();
+                push(pop() - op2);
+                break;
+            case '/':
+                op2 = pop();
+                if (op2 != 0.0)
+                    push(pop() / op2);
+                else
+                    printf("error: zero division\n");
+                break;
+            case '\n':
+                printf("\t%.8g\n", pop());
+                break;
+            default:
+                printf("error: unknown command %s\n", s);
+                break;
+        }
+    }
     return 0;
 }
 
-/* taken from example in Ch 4, Sec 4.1, Pg 69 */
-/* getline: get line into s, return length */
-int _getline(char s[], int lim) {
-    int c, i;
+#define MAXVAL 100 /* maximum depth of val stack */
 
-    i = 0;
-    while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
-        s[i++] = c;
+int sp = 0; /* next free stack position */
+double val[MAXVAL]; /* value stack */
 
-    if (c == '\n')
-        s[i++] = c;
-
-    s[i] = '\0';
-    return i;
+/* push: push f onto value stack */
+void push(double f) {
+    if (sp < MAXVAL)
+        val[sp++] = f;
+    else
+        printf("error: stack full, can't push %g\n", f);
 }
 
-/* taken from example in Ch 4, Sec 4.2, Pg 71 */
-/* atof: convert string s to double */
-double atof (char s[]) {
-    double val, power;
-    int i, sign;
-
-
-    for (i = 0; isspace(s[i]); i++) /* skip whitespace */
-        ;
-    sign = (s[i] == '-') ? -1 : 1;
-    if (s[i] == '+' || s[i] == '-')
-        i++;
-    for (val = 0.0; isdigit(s[i]); i++)
-        val = 10.0 * val + (s[i] - '0');
-    if (s[i] == '.')
-        i++;
-    for (power = 1.0; isdigit(s[i]); i++) {
-        val = 10.0 * val + (s[i] - '0');
-        power *= 10.0;
+/* pop: pop and return top value from stack */
+double pop(void) {
+    if (sp > 0)
+        return val[--sp];
+    else {
+        printf("error: stack empty\n");
+        return 0.0;
     }
-    return sign * val / power;
 }
+
+#include <ctype.h>
+
+int getch(void);
+void ungetch(int);
+
+/* getop: get next operator or numeric operand */
+int getop(char s[]) {
+    int i, c;
+
+    while ((s[0] = c = getch()) == ' ' || c == '\t')
+        ;
+    s[1] = '\0';
+    if (!isdigit(c) && c != '.')
+        return c; /* not a number */
+    i = 0;
+    if (isdigit(c)) /* collect integer part */
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    if (c == '.') /* collect fraction part */
+        while (isdigit(s[++i] = c =getch()))
+            ;
+    s[i] = '\0';
+    if (c != EOF)
+        ungetch(c);
+    return NUMBER;
+
+}
+
+#define BUFSIZE 100
+
+char buf[BUFSIZE]; /* buffer for ungetch */
+int bufp = 0; /* next free position in buf */
+
+int getch(void) { /* get a (possibly pushed back) character */
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) { /* push character back on input */
+    if (bufp >= BUFSIZE)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] = c;
+}
+
