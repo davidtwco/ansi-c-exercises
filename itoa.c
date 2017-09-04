a9afbb79c8ae45602441d358e7448150fe0406b9
David Wood <david@davidtw.co>
2017-09-04 19:31:25 +0100

    [Ex3-4] In a two's complement number representation, our version of itoa does not handle the largest negative number, that is, the value of n equal to -(2 to the power wordsize-1). Explain why not. Modify it to print that value correctly, regardless of the machine on which it runs.

--- itoa.c
+++ itoa.c
@@ -1,16 +1,64 @@
-/* example from Ch 3, Sec 3.6, Pg 64 */
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_SIZE 1000
+
+void itoa(int n, char s[]);
+void reverse(char s[]);
+
+/* original version of itoa did not work with the largest
+ * two's complement number as:
+ *
+ * two's complement uses the first bit for indicating whether
+ * the number is negative or not.
+ *
+ * The result of this is that the largest negative number
+ * that can be represented (in say, eight bits) is:
+ *
+ * -128 = 1000_0000
+ *
+ * whereas the largest positive is:
+ *
+ * 127 = 0111_1111
+ *
+ * Therefore the absolute value of the largest negative number
+ * cannot be stored in the same signed type. */
+
+int main() {
+    signed largest = INT_MIN;
+    char largestString[MAX_SIZE];
+
+    itoa(largest, largestString);
+    printf("%d as string: %s\n", largest, largestString);
+
+    return 0;
+}
+
 /* itoa: convert n to characters in s */
 void itoa(int n, char s[]) {
     int i, sign;
+    sign = n;
 
-    if ((sign = n) < 0) /* record sign */
-        n = -n; /* make n positive */
     i = 0;
     do { /* generate digits in reverse order */
-        s[i++] = n % 10 + '0'; /* get next digit */
-    } while ((n /= 10) > 0); /* delete it */
+        s[i++] = abs(n % 10) + '0'; /* get next digit */
+    } while (n /= 10); /* delete it */
     if (sign < 0)
         s[i++] = '-';
     s[i] = '\0';
     reverse(s);
+}
+
+/* taken from example in Ch 3, Sec 3.5, Pg 62 */
+/* reverse: reverse string s in place */
+void reverse(char s[]) {
+    int c, i, j;
+
+    for (i = 0, j = strlen(s) - 1; i < j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
 }
