41422c9c1c4c2358262aa5e8d8fd25332e383e46
David Wood <david@davidtw.co>
2017-09-09 12:25:58 +0100

    [Ex5-9] Rewrite the routines day_of_year and month_day with pointers instead of indexing.

--- dayOfYear.c
+++ dayOfYear.c
@@ -3,11 +3,10 @@ static char daytab[2][13] = {
     {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
 };
 
-/* example from Ch 5, Sec 5.7, Pg 111 */
-
 /* day_of_year: set day of year from month & day */
 int day_of_year(int year, int month, int day) {
     int i, leap;
+    char *p;
 
     if (year < 1 || month < 1 || month > 12 || day < 1)
         return -1;
@@ -16,14 +15,19 @@ int day_of_year(int year, int month, int day) {
     if (day > daytab[leap][month])
         return -1;
 
-    for (i = 1; i < month; i++)
-        day += daytab[leap][i];
+    p = &daytab[leap][1];
+
+    for (i = 1; i < month; i++) {
+        day += *p;
+        ++p;
+    }
     return day;
 }
 
 /* month_day: set month, day from day of year */
 int month_day(int year, int yearday, int *pmonth, int *pday) {
     int i, leap;
+    char *p;
 
     if (year < 1 || yearday < 1)
         return -1;
@@ -32,8 +36,12 @@ int month_day(int year, int yearday, int *pmonth, int *pday) {
     if ((leap && yearday > 356) || (!leap && yearday > 355))
         return -1;
 
-    for (i = 1; yearday > daytab[leap][i]; i++)
-        yearday -= daytab[leap][i];
+    p = &daytab[leap][1];
+
+    for (i = 1; yearday > *p; i++) {
+        yearday -= *p;
+        ++p;
+    }
     *pmonth = i;
     *pday = yearday;
     return 0;
