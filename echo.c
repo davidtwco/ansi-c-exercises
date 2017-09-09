20cdf3ac3928a1bbd413eaaf5463d8f3bbcba0af
David Wood <david@davidtw.co>
2017-09-09 12:30:21 +0100

    Updated echo example.

--- echo.c
+++ echo.c
@@ -2,12 +2,10 @@
 
 /* example from Ch 5, Sec 5.10, Pg 115 */
 
-/* echo command line arguments; 1st version */
+/* echo command line arguments; 2st version */
 int main(int argc, char *argv[]) {
-    int i;
-
-    for (i = 1; i < argc; i++)
-        printf("%s%s", argv[i], (i < argc - 1) ? " " : "");
+    while(--argc > 0)
+        printf("%s%s", *++argv, (argc > 1) ? " " : "");
     printf("\n");
     return 0;
 }
