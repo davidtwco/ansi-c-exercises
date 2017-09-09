ea50d4bd2014903737a9e0c31d9ccba32e69833c
David Wood <david@davidtw.co>
2017-09-09 12:40:59 +0100

    Updated find example.

--- find.c
+++ find.c
@@ -5,21 +5,42 @@
 
 int _getline(char *line, int max);
 
-/* example from Ch 5, Sec 5.10, Pg 116 */
+/* example from Ch 5, Sec 5.10, Pg 117 */
 
 /* find: print lines that match pattern from 1st arg */
 int main(int argc, char *argv[]) {
     char line[MAXLINE];
-    int found = 0;
+    long lineno = 0;
+    int c, except = 0, number = 0, found = 0;
 
-    if (argc != 2)
-        printf("Usage: find pattern\n");
+    while (--argc > 0 && (*++argv)[0] == '-')
+        while ((c = *++argv[0]))
+            switch (c) {
+                case 'x':
+                    except = 1;
+                    break;
+                case 'n':
+                    number = 1;
+                    break;
+                default:
+                    printf("find: illegal option %c\n", c);
+                    argc = 0;
+                    found = -1;
+                    break;
+            }
+
+    if (argc != 1)
+        printf("Usage: find -x -n pattern\n");
     else
-        while(_getline(line, MAXLINE) > 0)
-            if (strstr(line, argv[1]) != NULL) {
+        while(_getline(line, MAXLINE) > 0) {
+            lineno++;
+            if ((strstr(line, *argv) != NULL) != except) {
+                if (number)
+                    printf("%ld:", lineno);
                 printf("%s", line);
                 found++;
             }
+        }
     return found;
 }
 
