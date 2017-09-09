be40cb481a4ed2aa54811cbd013158add67b9ff0
David Wood <david@davidtw.co>
2017-09-09 09:43:01 +0100

    Updated strcpy example.

--- strcpy.c
+++ strcpy.c
@@ -1,9 +1,7 @@
 /* example from Ch 5, Sec 5.5, Pg 105 */
 
-/* strcpy: copy t to s: pointer version 1 */
+/* strcpy: copy t to s: pointer version 2 */
 void strcpy(char *s, char *t) {
-    while ((*s = *t) != '\0') {
-        s++;
-        t++;
-    }
+    while ((*s++ = *t++) != '\0')
+        ;
 }
