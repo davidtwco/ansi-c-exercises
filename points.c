efb27a74b7388df1df33eaba107cae109c10afac
David Wood <david@davidtw.co>
2017-09-09 21:27:29 +0100

    Added addpoint example.

--- points.c
+++ points.c
@@ -35,3 +35,10 @@ struct point makepoint(int x, int y) {
     temp.y = y;
     return temp;
 }
+
+/* addpoint: add two points */
+struct point addpoint(struct point p1, struct point p2) {
+    p1.x += p2.x;
+    p1.y += p2.y;
+    return p1;
+}
