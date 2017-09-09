cda1b8bcca9b853fd0ee55a925f292ecb7cc5288
David Wood <david@davidtw.co>
2017-09-09 21:29:20 +0100

    Added ptinrect example.

--- points.c
+++ points.c
@@ -42,3 +42,9 @@ struct point addpoint(struct point p1, struct point p2) {
     p1.y += p2.y;
     return p1;
 }
+
+/* ptinrect: return 1 if p in r, 0 if not */
+int ptinrect(struct point p, struct rect r) {
+    return p.x >= r.pt1.x && p.x < r.pt2.x
+        && p.y >= r.pt1.y && p.y < r.pt2.y;
+}
