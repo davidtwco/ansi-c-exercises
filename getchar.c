37afc889a5f39ccc360efc51aa47f7ba9596309d
David Wood <david@davidtw.co>
2017-09-10 19:16:02 +0100

    Added simple buffered getchar example.

--- getchar.c
+++ getchar.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include "unistd.h"
 
-/* example from Ch 8, Sec 8.2, Pg 171 */
+/* example from Ch 8, Sec 8.2, Pg 172 */
 
-/* getchar: unbuffered single character input */
+/* getchar: simple buffered version */
 int getchar(void) {
-    char c;
-    return (read(0, &c, 1) == 1) ? (unsigned char) c : EOF;
+    static char buf[BUFSIZ];
+    static char *bufp = buf;
+    static int n = 0;
+
+    if (n == 0) { /* buffer is empty */
+        n = read(0, buf, sizeof buf);
+        bufp = buf;
+    }
+    return (--n >= 0) ? (unsigned char) *bufp++ : EOF;
 }
