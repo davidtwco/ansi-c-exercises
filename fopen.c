24a5caef9b3be8807fd44642c7edea16caf4d21f
David Wood <david@davidtw.co>
2017-09-10 20:11:00 +0100

    [Ex8-4] The standard library function int fseek(FILE *fp, long offset, int origin) is identical to lseek except that fp is a file pointer instead of a file descriptor and the return value is an int status, not a position. Write fseek. Make sure that your fseek coordinates properly with the buffering done for the other functions of the library.

--- fopen.c
+++ fopen.c
@@ -255,6 +255,43 @@ int fclose(FILE *f) {
     return close(fd);
 }
 
+#define SEEK_SET 0
+#define SEEK_CUR 1
+#define SEEK_END 2
+
+int fseek(FILE *f, long offset, int whence) {
+    int result;
+
+    if ((f->flag & _UNBUF) == 0 && f->base != NULL) {
+        /* deal with buffering */
+        if (f->flag & _WRITE) {
+            /* writing, so flush buffer */
+            if (fflush(f))
+                return EOF;  /* from 8-3 */
+        } else if (f->flag & _READ) {
+            if (whence == SEEK_CUR) {
+                /* fix offset so that it's from the last
+                 * character the user read (not the last
+                 * character that was actually read) */
+                if (offset >= 0 && offset <= f->cnt) {
+                    /* easy shortcut */
+                    f->cnt -= offset;
+                    f->ptr += offset;
+                    f->flag &= ~_EOF; /* see below */
+                    return 0;
+                } else
+                    offset -= f->cnt;
+            }
+            f->cnt = 0;
+            f->ptr = f->base;
+        }
+    }
+    result = (lseek(f->fd, offset, whence) < 0);
+    if (result == 0)
+        f->flag &= ~_EOF; /* if successful, clear EOF flag */
+    return result;
+}
+
 int main() {
     int c;
     while ((c = getchar()) != 'x') {
